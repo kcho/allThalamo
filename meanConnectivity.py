5975555302b8b7cb884c3f986bc446d5d6023da7
kcho <cke8671@hotmail.com>
2013-03-07 17:38:21 -0500

    first commit

--- meanConnectivity.py
+++ meanConnectivity.py
@@ -14,8 +14,8 @@ import sys
 import commands
 import re
 import glob
+import pickle
 
-subdirectory = 'segmentation/with_T1_freesurfer/segmentation'
 
 class Data:
     '''
@@ -38,15 +38,19 @@ def main():
                 for item in subjectList.readlines()]
 
     #add 'Mean connectivity calculation' jobs to jobList
-    for subject in 'NOR01_LSJ','NOR02_JIY':#subjects:
-        print '\t\t\t***'+subject
-        data=meanConnectivityCalculation(subject)
+    jobs = [(subject,parallel.submit(meanConnectivityCalculation,(subject,),(fslstats,getGroup,),("commands",))) for subject in subjects]
 
-        #writing header to the log file
-        #with open('../logs/meanConnectivity.txt','w') as text:
-        #    text.write(''.join(data))
-        with open('../logs/meanConnectivity.txt','a') as text:
-            text.write(''.join(data))
+    for subject,job in jobs:
+        print subject
+        job()
+
+   # for subject in 'NOR01_LSJ','NOR02_JIY':#subjects:
+   #     print '\t\t\t***'+subject
+   #     data=meanConnectivityCalculation(subject)
+
+   #     #writing header to the log file
+   #     #with open('../logs/meanConnectivity.txt','w') as text:
+   #     #    text.write(''.join(data))
 
 def getGroup(subject):
     if 'CHR' in subject:
@@ -62,6 +66,7 @@ def meanConnectivityCalculation(subject):
     it calculates mean connectivity
     between thalamus and each cortex.
     '''
+    subdirectory = 'segmentation/with_T1_freesurfer/segmentation'
     #list to return
     toReturn=[]
 
@@ -76,12 +81,13 @@ def meanConnectivityCalculation(subject):
                 'OCC','OFC','PC','SMC'
 
         fullDirectory='../{group}/{subject}/{etc}/{side}'.format(
-           group=group, subject=subject,
+           group=group.lower(), subject=subject,
            etc=subdirectory,
            side=side)
 
         #for all thresholds and for all images
         for thr in range(10,90,10):
+            print str(thr),
             toReturn.append(subject)
             toReturn.append(group)
             toReturn.append(side)
@@ -89,6 +95,7 @@ def meanConnectivityCalculation(subject):
             eachTotalConnectivity=[]
             sumConnectivityForAll=0
             for layerNumber,img in zip(range(1,9),imgSequence):
+                print '.',
                 '''
                 fslstats for each layer number from 1 to 8
                 fslstats -V : output <voxels> <volume>
@@ -98,37 +105,40 @@ def meanConnectivityCalculation(subject):
 
 
                 #mean connectivity of the image
-                meanInImage=commands.getoutput(
-                        'fslstats {inputs} {option}'.
-                        format(
-                    inputs=fullDirectory+'/'+\
-                            str(thr)+'thrP/'+\
-                            str(thr)+'_seeds_to_'+sside+'_'+\
-                            img+'.nii.gz',
-                    option='-M'))
+                img=fullDirectory+'/'+str(thr)+'thrP/'+\
+                str(thr)+'_seeds_to_'+sside+'_'+img+'.nii.gz'
+                #############
+                #############
+                meanInImage=fslstats(img,'-M')
                 #volume of the image
-                imageVolume=commands.getoutput(
-                        'fslstats {inputs} {option}'.
-                        format(
-                    inputs=fullDirectory+'/'+\
-                            str(thr)+'thrP/'+\
-                            str(thr)+'_seeds_to_'+sside+'_'+\
-                            img+'.nii.gz',
-                    option='-V')).split(' ')
+                imageVolume=fslstats(img,'-V')
                 #all connectivity values summed up
-                totalConnectivity=float(meanInImage)/float(imageVolume[0])
+                totalConnectivity=meanInImage*imageVolume
                 eachTotalConnectivity.append(totalConnectivity)
                 sumConnectivityForAll+=totalConnectivity
 
             # Divide each total modality connection by
             # sum of connections
-            toReturn.append([item/sumConnectivityForAll\
-                    for item in eachTotalConnectivity])
+            toReturn.append('\t'.join([str(item/sumConnectivityForAll)\
+                    for item in eachTotalConnectivity]))
 
             # Reset for loop
             sumConnectivityForAll=0
             eachTotalConnectivity=[]
-    return toReturn
+
+    with open('../logs/meanConnectivity.txt','a') as text:
+        text.write('\t'.join(data))
+        text.write('\n')
+
+def fslstats(inputs,option):
+    output=commands.getoutput(
+    'fslstats {inputs} {option}'.format(
+                inputs=inputs,
+                option=option))
+    if option=='-V':
+        return float(output.split(' ')[0])
+    elif option=='-M':
+        return float(output)
 
 if __name__=='__main__':
     main()
